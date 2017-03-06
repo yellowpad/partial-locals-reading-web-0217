c7562c49de7fdb87304f6ec5a939e9a996f3fbdd
yellowpad <zayneabraham1@gmail.com>
2017-03-06 15:33:28 -0500

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	
   def show
     @post = Post.find(params[:id])
     @author = @post.author
@@ -7,4 +8,5 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
 end
